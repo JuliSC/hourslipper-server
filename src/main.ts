82ace2f8ff7920c8bbab764ab3a9ae0b0c350f34
Julius Stenbæk Christoffersen <julius@adnudging.com>
2022-12-18 15:19:35 +0100

    Deployment ready

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:8080",
+    origin: [
+      "http://localhost:8080",
+      "3.75.158.163",
+      "3.125.183.140",
+      "35.157.117.28",
+    ],
   });
 
   app.setGlobalPrefix("api");
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
